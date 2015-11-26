a13b21dce89d0b64ce02144e837eb933682f094e
Marcio Barroso <mbarroso@inmetrics.com.br>
2015-11-25 22:52:36 -0300

    prototype

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,13 +1,5 @@
 $(document).ready(function(){
 
-	$("#btn-minimal").click(function(){
-		$("div.minimal").hide();
-		$("div.main").show();
-	});
 
-	$("#btn-main").click(function(){
-		$("div.minimal").show();
-		$("div.main").hide();
-	});
 
 });
