c5bfba87096c993e492dc3dc4b4b9b7cbd7a68e8
Marcio Barroso <mbarroso@inmetrics.com.br>
2015-11-26 03:12:38 -0300

    add php script for google

--- resources/data/google.php
+++ resources/data/google.php
@@ -3,11 +3,34 @@
 	// all responses must be an XML
 	// header('Content-type: application/xml');
 
-	$GOOGLE_API_KEY = "AIzaSyA0t4XNY5bRhgy1SWPXbWjyCTJsqybFRHs";
+	define("GOOGLE_API_KEY", "AIzaSyA0t4XNY5bRhgy1SWPXbWjyCTJsqybFRHs");
 
 	function search() {
 		$query = $_GET["query"];
-		$latitude = $_GET["query"];
+		$latitude = $_GET["latitude"];
+		$longitude = $_GET["longitude"];
+
+		$url = "https://maps.googleapis.com/maps/api/place/nearbysearch/xml?";
+		$url .= "location=" . $latitude . "," . $longitude;
+		$url .= "&radius=5000&query=" . $query;
+		$url .= "&key=" . GOOGLE_API_KEY;
+
+		$xml = file_get_contents($url);
+		var_dump($xml);
 	}
 
-	print_r($_GET);
+	function error() {
+		$result = "<google>\n\t<result>error</result>\n</google>";
+		var_dump($result);
+	}
+
+	if( isset($_GET["action"]) ) {
+		switch( $_GET["action"] ) {
+			case "search":
+				search();
+				break;
+			default:
+				error();
+				break;
+		}
+	}
