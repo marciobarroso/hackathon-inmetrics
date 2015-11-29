f1dbf2e1764f41496ea76fff53b3b5035388431c
Marcio Barroso <mbarroso@inmetrics.com.br>
2015-11-29 15:38:19 -0200

    design adjust

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
 			<form>
 				<div class="row">
 					<div class="col-md-3 col-sm-2 col-lg-4 col-xs-2"></div>
-					<div class="col-md-5 col-sm-5 col-lg-3 col-xs-5">
+					<div class="col-md-5 col-sm-7 col-lg-3 col-xs-7">
 						<input type="text" id="input-search" placeholder="O que você procura?" class="form-control">
 					</div>
 					<div class="col-md-1 col-sm-1 col-lg-1 col-xs-1 form-group">
